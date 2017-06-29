2cfe338e6a78de27d1745aa84c95bbc214be7ac2
mt <oc.mt@hotmail.com>
2017-06-29 20:51:51 +0800

    ..

---
+++ src/main/java/org/vinci/math/ProgramEntry.java
@@ -0,0 +1,12 @@
+package org.vinci.math;
+
+import jodd.datetime.JDateTime;
+
+/**
+ * Created by Jao on 2017/6/26.
+ */
+public class ProgramEntry {
+    public static void main(String... args) throws Throwable {
+        System.out.println(new JDateTime().getTimeInMillis());
+    }
+}
